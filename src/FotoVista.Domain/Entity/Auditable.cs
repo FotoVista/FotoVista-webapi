ad6261d418d5b21a4642b9c6678f64c97d463d3c
Mirfayz <muiilhom@gmail.com>
2023-09-09 20:47:36 +0500

    Added Auditable

---
+++ src/FotoVista.Domain/Entity/Auditable.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FotoVista.Domain.Entities;
+
+public abstract class Auditable : BaseEntity
+{
+    [Required]
+    public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+
+    [Required]
+    public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+}
