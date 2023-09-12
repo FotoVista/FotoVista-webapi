4572716ff1bc71702fb94920897b46db43be9955
Mirfayz <muiilhom@gmail.com>
2023-09-12 11:34:00 +0500

    Added DataAccessConfiguration

---
+++ src/FotoVista-WebApi/Configurations/DataAccessConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using FotoVista.DataAccess.DbContexts;
+using Microsoft.EntityFrameworkCore;
+namespace FotoVista_WebApi.Configurations;
+
+public class DataAccessConfiguration
+{
+    private readonly IConfiguration _configuration;
+
+    public DataAccessConfiguration(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public DbContextOptions<AppDbContext> GetDbContextOptions()
+    {
+        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseNpgsql(connectionString); 
+
+        return optionsBuilder.Options;
+    }
+}
+
