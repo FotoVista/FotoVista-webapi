c3e40263076ecff583cb2ff9fb0e05c386cf62e3
Mirfayz <muiilhom@gmail.com>
2023-10-10 09:58:16 +0500

    Added Comment Dto

---
+++ src/FotoVista.Service/DTOs/Comments/CommentCreateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FotoVista.Service.DTOs;
+
+public class CommentCreateDto
+{
+    public long PostId { get; set; }
+
+    public long UserId { get; set; }
+
+    public string Text { get; set; } = string.Empty;
+}
---
+++ src/FotoVista.Service/DTOs/Comments/CommentResultDto.cs
@@ -0,0 +1,16 @@
+﻿namespace FotoVista.Service.DTOs;
+
+public class CommentResultDto
+{
+    public long Id { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string UserName { get; set; } = string.Empty;
+
+    public string ImagePath { get; set; } = string.Empty;
+
+    public string Text { get; set; } = string.Empty;
+}
