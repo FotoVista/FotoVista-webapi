8215d28d9906f3c87af8ce9fcb3312dec9bf7865
Mirfayz <muiilhom@gmail.com>
2023-10-10 09:54:41 +0500

    Added Following Entity

---
+++ src/FotoVista.Domain/Entity/Followings/Following.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FotoVista.Domain.Entity;
+
+public class Following : Auditable
+{
+    [Required]
+    public long UserId { get; set; }
+
+    [Required]
+    [Column("following_user_id")]
+    public long FollowingUserId { get; set; }
+
+    [Required]
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [ForeignKey("UserId")]
+    public UserEntity User { get; set; } = default!;
+
+    [ForeignKey("FollowingUserId")]
+    public UserEntity FollowingUser { get; set; } = default!;
+}
