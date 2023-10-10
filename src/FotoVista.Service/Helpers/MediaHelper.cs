4e14de000099b544ab6edf911032fb0c33a085d9
Mirfayz <muiilhom@gmail.com>
2023-10-10 10:00:22 +0500

    Added Helpers

---
+++ src/FotoVista.Service/Helpers/MediaHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace FotoVista.Service.Helpers
+{
+    public class MediaHelper
+    {
+        public static string MakeImageName(string filename)
+        {
+            FileInfo fileInfo = new FileInfo(filename);
+
+            string extension = fileInfo.Extension;
+            string name = "IMG_" + Guid.NewGuid() + extension;
+
+            return name;
+        }
+        public static string[] GetImageExtensions()
+        {
+            return new string[]
+            {
+                // .JPG files
+                ".jpg", ".jpeg",
+                // .PNG files
+                ".png",
+                // .BMP files
+                ".bmp",
+                // .SVG files
+                ".svg",
+                // .HEIC files
+                ".HEIC"
+            };
+        }
+    }
+}
---
+++ src/FotoVista.Service/Helpers/TimeHelper.cs
@@ -0,0 +1,13 @@
+﻿using FotoVista.Domain.Constants;
+
+namespace FotoVista.Service.Helpers;
+
+public class TimeHelper
+{
+    public static DateTime GetDateTime()
+    {
+        var datetime = DateTime.UtcNow;
+        datetime.AddHours(TimeConstants.UTC);
+        return datetime;
+    }
+}
