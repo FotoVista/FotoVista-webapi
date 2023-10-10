e81f62b987b1ae684e4f541615112271b8b5a3e1
Mirfayz <muiilhom@gmail.com>
2023-10-10 09:52:57 +0500

    Added UnitOfWork

---
+++ src/FotoVista.DataAccess/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using FotoVista.DataAccess.IRepository;
+using FotoVista.Domain.Entity;
+using static NPOI.HSSF.Util.HSSFColor;
+
+namespace FotoVista.DataAccess.UnitOfWork;
+
+public interface IUnitOfWork
+{
+
+    IRepository<UserEntity> UserRepository { get; }
+    IRepository<RoleEntity> RoleRepository { get; }
+    IRepository<PostEntity> PostRepository { get; }
+    IRepository<CommentEntity> CommentRepository { get; }
+    IRepository<LikeEntity> LikeRepository { get; }
+    IRepository<HashtagEntity> HashtagRepository { get; }
+    IRepository<PostHashtagEntity> PostHashtagRepository { get; }
+    IRepository<FollowerEntity> FollowerRepository { get; }
+    IRepository<Following> FollowingRepository { get; }
+
+
+    Task<int> SaveAsync();
+}
---
+++ src/FotoVista.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using FotoVista.DataAccess.DbContexts;
+using FotoVista.DataAccess.IRepository;
+using FotoVista.DataAccess.Repository;
+using FotoVista.Domain.Entity;
+
+namespace FotoVista.DataAccess.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public IRepository<UserEntity> UserRepository { get; }
+    public IRepository<RoleEntity> RoleRepository { get; }
+    public IRepository<PostEntity> PostRepository { get; }
+    public IRepository<CommentEntity> CommentRepository { get; }
+    public IRepository<LikeEntity> LikeRepository { get; }
+    public IRepository<HashtagEntity> HashtagRepository { get; }
+    public IRepository<PostHashtagEntity> PostHashtagRepository { get; }
+    public IRepository<FollowerEntity> FollowerRepository { get; }
+
+    public IRepository<Following> FollowingRepository { get; }
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+
+        UserRepository = new Repository<UserEntity>(_context);
+        RoleRepository = new Repository<RoleEntity>(_context);
+        PostRepository = new Repository<PostEntity>(_context);
+        CommentRepository = new Repository<CommentEntity>(_context);
+        LikeRepository = new Repository<LikeEntity>(_context);
+        HashtagRepository = new Repository<HashtagEntity>(_context);
+        PostHashtagRepository = new Repository<PostHashtagEntity>(_context);
+        FollowerRepository = new Repository<FollowerEntity>(_context);
+        FollowingRepository = new Repository<Following>(_context);
+    }
+
+    public async Task<int> SaveAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
+
