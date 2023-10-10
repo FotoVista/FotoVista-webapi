134b44c91bbc2cd6105e1f7fd9b12c85230d5719
Mirfayz <muiilhom@gmail.com>
2023-10-10 09:37:27 +0500

    Write Configurations in WebApi

---
+++ src/FotoVista-WebApi/Configurations/LayerConfigurations/DataAccessConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using FotoVista.DataAccess.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace FotoVista.WebApi.Configurations.LayerConfigurations;
+
+public static class DataAccessConfiguration
+{
+    public static void ConfigureDataAccess(this WebApplicationBuilder builder)
+    {
+        string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
+        
+        builder.Services.AddDbContext<AppDbContext>(options => 
+            options.UseNpgsql(connectionString));
+    }
+}
