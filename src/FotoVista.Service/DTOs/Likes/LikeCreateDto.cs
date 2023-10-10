eb4e5c2f3e710cc11f191328aabb66bba0891456
Mirfayz <muiilhom@gmail.com>
2023-10-10 09:59:19 +0500

    Added Like Dto

---
+++ src/FotoVista.Service/DTOs/Likes/LikeCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FotoVista.Service.DTOs;
+
+public class LikeCreateDto
+{
+    public long PostId { get; set; }
+
+    public long UserId { get; set; }
+}
---
+++ src/FotoVista.Service/DTOs/Likes/LikeResultDto.cs
@@ -0,0 +1,14 @@
+﻿namespace FotoVista.Service.DTOs;
+
+public class LikeResultDto
+{
+    public long UserId { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string UserName { get; set; } = string.Empty;
+
+    public string ImagePath { get; set; } = string.Empty;
+}
