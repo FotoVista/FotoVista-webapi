3a27c6f71a7027894da4fc810f20b93e593c5ecf
Mirfayz <muiilhom@gmail.com>
2023-10-10 09:56:56 +0500

    Added Auditable

---
+++ src/FotoVista.Domain/Entity/Auditable.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FotoVista.Domain.Entity;
+
+public class Auditable
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+    public bool IsDeleted { get; set; }
+}
