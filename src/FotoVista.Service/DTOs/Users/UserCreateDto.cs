c6059bbdff753f23b2cf1a6558efc81d472d154d
Mirfayz <muiilhom@gmail.com>
2023-10-10 10:00:01 +0500

    Added User Dto

---
+++ src/FotoVista.Service/DTOs/Users/UserCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+namespace FotoVista.Service.DTOs;
+
+public class UserCreateDto
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string UserName {  get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public IFormFile Image { get; set; } = default!;
+
+    public string Bio { get; set; } = string.Empty;
+}
---
+++ src/FotoVista.Service/DTOs/Users/UserResultDto.cs
@@ -0,0 +1,22 @@
+﻿namespace FotoVista.Service.DTOs;
+
+public class UserResultDto
+{
+    public long Id { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string UserName { get; set; } = string.Empty;
+
+    public string ProfilePictureUrl { get; set; } = string.Empty;
+
+    public string Bio { get; set; } = string.Empty;
+
+    public DateTime CreateAt { get; set; }
+
+    public DateTime UpdateAt { get; set; }
+
+    public string Role { get; set; } = string.Empty;
+}
