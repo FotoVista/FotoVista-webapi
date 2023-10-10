63bc4e940723ba2e84fbe3dd096fd4c52acfca49
Mirfayz <muiilhom@gmail.com>
2023-10-10 09:58:52 +0500

    Added Following Dto

---
+++ src/FotoVista.Service/DTOs/Followings/FollowingCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FotoVista.Service.DTOs;
+
+public class FollowingCreateDto
+{
+    public long UserId { get; set; }
+
+    public long FollowingUserId { get; set; }
+}
---
+++ src/FotoVista.Service/DTOs/Followings/FollowingResultDto.cs
@@ -0,0 +1,14 @@
+﻿namespace FotoVista.Service.DTOs;
+
+public class FollowingResultDto
+{
+    public long Id { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string UserName { get; set; } = string.Empty;
+
+    public string ImagePath { get; set; } = string.Empty;
+}
