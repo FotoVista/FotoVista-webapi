a9e442e9414a73a9af9c3d5c6baa51ad5fe81bc8
Mirfayz <muiilhom@gmail.com>
2023-10-10 09:59:46 +0500

    Added Post Dto

---
+++ src/FotoVista.Service/DTOs/Posts/PostCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FotoVista.Service.DTOs;
+
+public class PostCreateDto
+{
+    public long UserId {  get; set; }
+
+    public string Caption { get; set; } = string.Empty;
+
+    public IFormFile ?ImageUrl{ get; set; } = default!;
+}
---
+++ src/FotoVista.Service/DTOs/Posts/PostResultDto.cs
@@ -0,0 +1,20 @@
+﻿namespace FotoVista.Service.DTOs;
+
+public class PostResultDto
+{
+    public long UserId { get; set; }
+
+    public long PostId { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string UserName { get; set; } = string.Empty;
+
+    public string Caption { get; set; } = string.Empty;
+
+    public string ImageUrl { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; }
+}
