9eb413e74befaad604c2531d71824384c8a37627
Mirfayz <muiilhom@gmail.com>
2023-10-17 14:48:48 +0500

    Added configurations

---
+++ src/FotoVista-WebApi/Configurations/LayerConfigurations/ServiceLayerConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using FotoVista.Service.Interfaces;
+using FotoVista.Service.Services;
+
+namespace FotoVista.WebApi.Configurations.LayerConfigurations;
+
+public static class ServiceLayerConfiguration
+{
+    public static void ConfigureServiceLayer(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IAuthAdminService, AuthAdminService>();
+        //builder.Services.AddScoped<IAuthService, AuthService>();
+        builder.Services.AddScoped<ITokenAdminService, TokenAdminService>();
+        builder.Services.AddScoped<IMailSender, MailSender>();
+    }
+}
