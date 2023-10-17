40d4b3f2c1bef7b8261d3db43979e8c59a0d8479
Mirfayz <muiilhom@gmail.com>
2023-10-17 14:49:28 +0500

    F

--- src/FotoVista-WebApi/Program.cs
+++ src/FotoVista-WebApi/Program.cs
@@ -1,31 +1,35 @@
-using Microsoft.EntityFrameworkCore;
-using FotoVista.DataAccess.DbContexts;
 using FotoVista.WebApi.Configurations.LayerConfigurations;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+//builder.ConfigureServiceLayer();
 builder.ConfigureDataAccess();
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
+app.UseStaticFiles();
+//app.UseMiddleware<CrosOriginAccessMiddleware>();
+//app.UseMiddleware<ExceptionHandlerMiddleware>();
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
